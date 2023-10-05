3590402019367785d7c9d82db2024e4d772ade6c
unknown <mylekyanina@gmail.com>
2023-10-05 16:07:40 -0300

    agregando validaciones de formulario

--- js/contacto.js
+++ js/contacto.js
@@ -1,30 +1,90 @@
+let envio = document.querySelector('#registro');
 
-function registrate(){
-let email = prompt("Ingresá tu email para conocer los últimos eventos:");
-
-if ((email.indexOf('@') != -1)&&(email.indexOf('.com') != -1)){
-    alert('Se ha registrado el mail: ' + email + ' correctamente');
-    console.log("El email "+ email + "ha sido registrado exitosamente")
-}else{
-    alert('El email ingresado no es válido. Intentelo nuevamente')
-}
-}
-/* Chequeando uno por uno
- if (email.indexOf('@') != -1){
-    alert('puse @')
-}else{
-    alert('no puse @')
-}
-if(email.indexOf('.com') != -1){
-    alert('puse .com')
-}else{
-    alert('no puse .com')
-}
- */
-
-function MensajeEnviado(){
-    if ((document.getElementById('mensaje').value != "")&&(document.getElementById('email').value != "")&&(document.getElementById('apellido').value != "") ){
+envio.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if (validarNombre() && validarApellido() && validarEmail() && validarMensaje()){
         alert('Su mensaje ha sido enviado exitosamente');
+        return true;
+    }else{
+        alert('El mensaje no ha sido enviado. Verifique los campos');
+        return false;
+    }
+})
+
+
+//funciones para validar nombre y apellido 
+
+function validarNombre(e){
+    const nombre = document.getElementById('nombre').value;
+    //e.preventDefault();
+    const caracteresValidos = /^[ a-zA-ZñÑáéíóúüçÁÉÍÓÚÜÇ]+$/;
+    if (nombre != '' && nombre != null && caracteresValidos.test(nombre)){
+        colorValorValido('#nombre');
+        
+        return true;
+    }else{
+        colorValorInvalido('#nombre');
+        return false;
+    }
+}
+
+function validarApellido(e){
+    const apellido = document.getElementById('apellido').value;
+   // e.preventDefault();
+    const caracteresValidos = /^[ a-zA-ZñÑáéíóúüçÁÉÍÓÚÜÇ]+$/;
+    if (apellido != '' && apellido != null && caracteresValidos.test(apellido)){
+        colorValorValido('#apellido');
+        
+        return true;
+    }else{
+        colorValorInvalido('#apellido');
+        return false;
+    }
+}
+
+//función para validar email
+
+function validarEmail(e){
+    // e.preventDefault();
+     const email = document.getElementById('email').value;
+     const emailValido = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+     if (email != '' && email != null && emailValido.test(email)){
+         colorValorValido('#email');
+         return true;
+     }else{
+         colorValorInvalido('#email');
+         return false;
+     }
+     
+ }
+ //validar mensaje
+function validarMensaje(){
+    let msj = document.getElementById('mensaje').value;
+    if (msj != '' && msj != null && msj.length>10){
+        colorValorValido('#mensaje');
+        return true;
+    }else{
+        colorValorInvalido('#mensaje');
+        return false;
     }
 
 }
+
+//agregandole y sacandole las clases para los colores de campo
+// version simplificada?
+function colorValorValido(elemento){
+    document.querySelector(elemento).classList.add("valida");
+    document.querySelector(elemento).classList.remove("invalido");
+}
+function colorValorInvalido(elemento){
+    document.querySelector(elemento).classList.add("invalido");
+    document.querySelector(elemento).classList.remove("valida");  
+}
+
+
+
+document.querySelector('#email').addEventListener('blur', validarEmail);
+document.querySelector('#nombre').addEventListener('blur', validarNombre);
+document.querySelector('#apellido').addEventListener('blur', validarApellido);
+document.querySelector('#mensaje').addEventListener('blur', validarMensaje);
+
