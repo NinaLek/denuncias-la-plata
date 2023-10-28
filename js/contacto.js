a7598b6df4879300f0211c96f2612707e27c52dc
Nina <mylekyanina@gmail.com>
2023-10-28 16:19:53 -0300

    optimización de variables,fix local storage de usuarios, fix responsive

--- js/contacto.js
+++ js/contacto.js
@@ -6,105 +6,72 @@ const nombrev = document.querySelector('#nombre');
 const apellidov = document.querySelector('#apellido');
 const mensajev = document.querySelector('#mensaje');
 const btnSuscribite = document.getElementById('btn-suscribite');
+const mensajesEnviados = []; //arreglo para los mensajes
+const usuarios = []; //arreglo para los suscriptores
+const caracteresValidos = /^[ a-zA-ZñÑáéíóúüçÁÉÍÓÚÜÇ]+$/; //validación letras
+const emailValido = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;  //validación emails
 
 
-envio.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    if (validarNombre() && validarApellido() && validarEmail() && validarMensaje() && validarMotivo(e)){
-        let mensajeActual = new Mensaje(nombre.value,apellido.value,email.value,mensaje.value, motivoContacto.value);
-        mensajesEnviados.push(mensajeActual);
-        let mensajeJSON = JSON.stringify(mensajesEnviados);
-        mensajesEnviados.push(mensajeActual);
-        localStorage.setItem('mensajes',mensajeJSON);
-
-      //  alert('Su mensaje ha sido enviado exitosamente');
+envio.addEventListener('submit', (e)=>{ //al hacer click en el botón enviar
+    e.preventDefault();                 // detenemos el envío hasta validar los datos
+    if (validarNombre() && validarApellido() && validarEmail() && validarMensaje() && validarMotivo(e)){  //si todo es correcto se envia el formulario y se archiva en el local storage
+        let mensajeActual = new Mensaje(nombre.value,apellido.value,email.value,mensaje.value, motivoContacto.value); //creamos un objeto mensaje el cual enviaremos al array antes de enviarlo al Local Storage
+        mensajesEnviados.push(mensajeActual); //pusheamos el msj nuevo al array mensajeEnviados
+        let mensajeJSON = JSON.stringify(mensajesEnviados); // convertimos el arreglo a formato JSON
+        localStorage.setItem('mensajes',mensajeJSON); //enviamos el array en formato JSON al Local storage
             
-        /// creo que lo de sacar de ña base de datos es para el caso en que tuviesemos un json ya y tuviesemos que sacar los datos de ahi
-        let nuevoObjeto = JSON.parse(localStorage.getItem("mensajes"));//acá para crear uno desde el local storage nomás
-
+        //recupera los datos del local storage y los convierte en un objeto mensajeRecuperado
+        let mensajeRecuperado = JSON.parse(localStorage.getItem("mensajes"));//crea el objeto desde el local storage
 
         ///mostrar los datos ingresados
-        let nomIngresado = nuevoObjeto[nuevoObjeto.length-1].nombre;
-        let apeIngresado = nuevoObjeto[nuevoObjeto.length-1].apellido;
-        let emailIngresado = nuevoObjeto[nuevoObjeto.length-1].email;
-        let msjIngresado = nuevoObjeto[nuevoObjeto.length-1].mensaje;
-        let motIngresado = nuevoObjeto[nuevoObjeto.length-1].motivo;
-        
+        let nomIngresado = mensajeRecuperado[mensajeRecuperado.length-1].nombre;
+        let apeIngresado = mensajeRecuperado[mensajeRecuperado.length-1].apellido;
+        let emailIngresado = mensajeRecuperado[mensajeRecuperado.length-1].email;
+        let msjIngresado = mensajeRecuperado[mensajeRecuperado.length-1].mensaje;
+        let motIngresado = mensajeRecuperado[mensajeRecuperado.length-1].motivo;
+        //Se muestran los datos recuperados con el reemplazo del alert 'sweet alert' para que quede más lindo :D
         swal('Mensaje enviado exitosamente',`Nombre: ${nomIngresado} \n Apellido: ${apeIngresado} \n Email: ${emailIngresado} \n Motivo: ${motIngresado} \n Mensaje: ${msjIngresado}`, 'success' );
         return true;
-
     }else{
-
-        swal('Algo  no salió como esperabamos', 'El mensaje no ha sido enviado. Verifique que todos los campos tengan datos válidos', 'error');
+        // si falta completar algún dato o hubo algún dato inválido el formulario no se envía
+        swal('Algo no salió como esperabamos', 'El mensaje no ha sido enviado. Verifique que todos los campos tengan datos válidos', 'error');
         return false;
-
     }
 })
 
 
+//funciones para validar nombre y apellido    - falta unirlas, me está costando
 
-//funciones para validar nombre y apellido     ¡¡¡¿¿ CÓMO MEJORAR PARA NO REPETIR EL CODIGO??!!!
-
-function validarNombre(e){
-    //e.preventDefault();
-
-    const nombre = document.getElementById('nombre').value;
-    const caracteresValidos = /^[ a-zA-ZñÑáéíóúüçÁÉÍÓÚÜÇ]+$/;
-
-    if (nombre != '' && nombre != null && caracteresValidos.test(nombre)){
-        
-        colorValorValido('#nombre');
+function validarNombre(){  //validamos que sólo acepten letras
+    if (nombrev.value != '' && nombrev.value != null && caracteresValidos.test(nombrev.value)){
+        colorValorValido(nombrev); //si el valor es correcto el borde del campo input es verde
         return true;
-
     }else{
-
-        colorValorInvalido('#nombre');
+        colorValorInvalido(nombrev);  //si el valor es incorrecto el borde del campo input es rojo
         return false;
     }
 }
 
-function validarApellido(e){
-    // e.preventDefault();
-
-    const apellido = document.getElementById('apellido').value;
-    const caracteresValidos = /^[ a-zA-ZñÑáéíóúüçÁÉÍÓÚÜÇ]+$/;
-    
-    if (apellido != '' && apellido != null && caracteresValidos.test(apellido)){
-
-        colorValorValido('#apellido');
+function validarApellido(){
+    if (apellidov.value != '' && apellidov.value != null && caracteresValidos.test(apellidov.value)){
+        colorValorValido(apellidov);
         return true;
-
     }else{
-
-        colorValorInvalido('#apellido');
+        colorValorInvalido(apellidov);
         return false;
-
     }
 }
-
-
 
 
 //función para validar email
-
-function validarEmail(e){
-    // e.preventDefault();
-
-     const email = document.getElementById('email').value;
-     const emailValido = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-
-     if (email != '' && email != null && emailValido.test(email)){
-
-         colorValorValido('#email');
+function validarEmail(){
+     if (emailv.value != '' && emailv.value != null && emailValido.test(emailv.value)){
+         colorValorValido(emailv);
          return true;
-
      }else{
-
-         colorValorInvalido('#email');
+         colorValorInvalido(emailv);
          return false;
-
      }
-     
  }
 
 //funcion para validar el motivo
@@ -115,52 +82,42 @@ function validarMotivo(e){
     //default cuando no hay ningun motivo seleccionado
 
     if (motivo === 'motivo') {
-        colorValorInvalido('#motivoContacto')
+        colorValorInvalido(motiv);
         return false
     } else {
-        colorValorValido('#motivoContacto')
+        colorValorValido(motiv);
         return true
     }
 }
 
  //validar mensaje
-function validarMensaje(){
-
-    let msj = document.getElementById('mensaje').value;
-
-    if (msj != '' && msj != null && msj.length>10){
-
-        colorValorValido('#mensaje');
+function validarMensaje(){ // se valida que al menos el mensaje sea de 10 caracteres
+    if (mensajev.value != '' && mensajev.value != null && mensajev.value.length>10){
+        colorValorValido(mensajev);
         return true;
-
     }else{
-
-        colorValorInvalido('#mensaje');
+        colorValorInvalido(mensajev);
         return false;
-
     }
 
 }
 
 //agregandole y sacandole las clases para los colores de campo
-// version simplificada?
 
-function colorValorValido(elemento){
-    document.querySelector(elemento).classList.remove("limpio");
-
-    document.querySelector(elemento).classList.add("valida");
-    document.querySelector(elemento).classList.remove("invalido");
+function colorValorValido(elemento){ //agregando y quitando colores de validación a los inputs
+    elemento.classList.remove("limpio");
+    elemento.classList.add("valida");
+    elemento.classList.remove("invalido");
 }
 
 function colorValorInvalido(elemento){
-    document.querySelector(elemento).classList.remove("limpio");
-    document.querySelector(elemento).classList.add("invalido");
-    document.querySelector(elemento).classList.remove("valida");  
+    elemento.classList.remove("limpio");
+    elemento.classList.add("invalido");
+    elemento.classList.remove("valida");  
 }
 
 
 //Validar con blur
-
 emailv.addEventListener('blur', validarEmail);
 motiv.addEventListener('blur', validarMotivo);
 nombrev.addEventListener('blur', validarNombre);
@@ -168,7 +125,7 @@ apellidov.addEventListener('blur', validarApellido);
 mensajev.addEventListener('blur', validarMensaje);
 
 
-
+/////////// Acá arrancamos con los usuarios que se suscriban//////////////////
 
 function Usuario(nombre, apellido, email){
     this.nombre= nombre,
@@ -176,43 +133,53 @@ function Usuario(nombre, apellido, email){
     this.apellido =apellido
 }
 
-let usuarios = [];
-
 btnSuscribite.addEventListener('click', (e)=>{
-
+   
     let emailsusc = prompt('Ingresá tu mail para recibir nuestras novedades');
-    const emailValido = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-    const caracteresValidos = /^[ a-zA-ZñÑáéíóúüçÁÉÍÓÚÜÇ]+$/;
+    let nuevoUsuario = true;
+   
+    if(emailsusc != '' && emailsusc != null && emailValido.test(emailsusc)){ //capturamos el email ingresado por el prompt
+        //revisa la base de usuarios
 
-    if (emailsusc != '' && emailsusc != null && emailValido.test(emailsusc)){
-        let nombsusc= prompt('Ingresá tu nombre');
-        if (nombsusc != '' && nombsusc != null && caracteresValidos.test(nombsusc)){
-            let apesusc= prompt('Ingresá tu apellido');
-            if (apesusc != '' && apesusc != null && caracteresValidos.test(apesusc)){
-                let usuario= new Usuario(nombsusc,apesusc,emailsusc);//instancio un nuevo usuario
-                usuarios.push(usuario);//se lo mando al arreglo de usuarios
-                let usuariosJSON= JSON.stringify(usuarios);
-                localStorage.setItem('usuarios',usuariosJSON);//se supone que acá se mandó al local storage
-               // alert('Felicidades, ' + nombsusc + ' ' + apesusc + ' ya estás registrada en nuestra base de datos');
-               swal('¡Felicitaciones!',`${nombsusc} ${apesusc} ya estás registrada/o en nuestra base de datos!`,'success');
-                let msjPantalla= `Nombre: ${nombsusc} <br> Apellido: ${apesusc} <br> Email: ${emailsusc}`;
-                document.querySelector('#ventana-ingresados').innerHTML= msjPantalla;
-               
-                return true; 
+        if(usuarios.length > 0){ //si tiene algo el array de usuarios guardados lo revisamos
+            const usuariosGuardados = JSON.parse(localStorage.getItem('usuarios'));
+            console.log('cantidad de usuarios guardados' + usuariosGuardados.length);
+            for(var i=0; i<usuariosGuardados.length;i++){
+                if(usuariosGuardados[i].email == emailsusc){  //si el mail guardado en esa posición es igual al ingresado
+                    nuevoUsuario = false; //ya no es un usuario nuevo, se le comunica los datos ingresados en pantalla
+                    swal('Momento!', 'Este email ha sido registrado anteriormente, chequeá los datos ingresados', 'error');
+                    let msjPantalla = `Nombre: ${usuariosGuardados[i].nombre} <br> Apellido: ${usuariosGuardados[i].apellido} <br> Email: ${usuariosGuardados[i].email}`
+                    document.querySelector('#ventana-ingresados').innerHTML= msjPantalla;
+                }
+                if(!nuevoUsuario){//corta cuando encuentre una coincidencia
+                    break;
+                }
             }
         }
+        if(usuarios == null || nuevoUsuario){  //si no habia usuarios o no estaba registrado el mail agrega otro
+            //si el array estaba vacío o si no lo encuentra sigue para ingresar un usuario nuevo
+            let nombsusc= prompt('Ingresá tu nombre');
+            if (nombsusc != '' && nombsusc != null && caracteresValidos.test(nombsusc)){ //valida el nombre ingresado
+                let apesusc= prompt('Ingresá tu apellido');
+                if (apesusc != '' && apesusc != null && caracteresValidos.test(apesusc)){  //valida el apellido ingresado
+                    let usuario= new Usuario(nombsusc,apesusc,emailsusc);//instancio un nuevo objeto usuario
+                    usuarios.push(usuario);//se lo mando al arreglo de usuarios
+                    let usuariosJSON= JSON.stringify(usuarios);// se convierte a formato JSON
+                    localStorage.setItem('usuarios',usuariosJSON);//se manda al local storage
+                //mostrando los datos desde la pagina y no el local storage
+                swal('¡Felicitaciones!',`${nombsusc} ${apesusc} ya estás registrada/o en nuestra base de datos!`,'success');
+                let msjPantalla= `Nombre: ${nombsusc} <br> Apellido: ${apesusc} <br> Email: ${emailsusc}`;
+                document.querySelector('#ventana-ingresados').innerHTML= msjPantalla;
+               return true; 
+                }
+            }
+        }
+    }else{ //si se ingresa un email no válido
+        swal('Ha ocurrido un error', 'Por favor revise sus datos','error');
+        return false;
     }
-    else{
-
-        swal('Ha ocurrido un error. Por favor revise sus datos','error');
-    }
-
 });
   
-// chequeando que el local storage ande
-//console.log(localStorage.getItem('usuarios'));// muestra el ingresado antes de la recarga
-
-
 //localstorage para los mensajes
 //objeto mensaje
 
@@ -226,20 +193,12 @@ class Mensaje {
     }
 }
 
-let mensajesEnviados = [];
 
-limpiar.addEventListener('click', ()=>{   ///¿Cómo se puede hacer para que al limpiar se salga lo que se muestra?
 
-    document.querySelector('#ventana-ingresados').innerHTML='';
-   
+limpiar.addEventListener('click', ()=>{   //botón para limpiar tanto los campos del formulario como la pantalla de los datos ingresados
+    document.querySelector('#ventana-ingresados').innerHTML='';   
     const  inputs = [emailv, nombrev, apellidov, mensajev, motiv];
     inputs.forEach( function(element){
         element.classList.add('limpio');
     });
-    // emailv.classList.add('limpio');
-    // nombrev.classList.add('limpio');
-    // apellidov.classList.add('limpio');
-    // mensajev.classList.add('limpio');
-    // motiv.classList.add('limpio');
-
 })
